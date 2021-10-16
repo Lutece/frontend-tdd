b23e028096f36053aacef15a39f87006cc718af0
Horsicode <horsicode@gmail.com>
2021-10-16 10:36:33 +0000

    add test

--- examples/basic-environment/sum.test.js
+++ examples/basic-environment/sum.test.js
@@ -1,4 +1,4 @@
-const sum = require("./sum");
+const sum = require("./sum")
 
 test("adds 1 + 2 to equal 3", () => {
   expect(sum(1, 2)).toBe(3);
@@ -7,7 +7,7 @@ test("adds 1 + 2 to equal 3", () => {
 const myBeverage = {
   delicious: true,
   sour: false
-};
+}
 
 describe("테스트를 그룹화하는 역할을 합니다.", () => {
   test("is delicious", () => {
@@ -18,3 +18,54 @@ describe("테스트를 그룹화하는 역할을 합니다.", () => {
     expect(myBeverage.sour).toBeFalsy();
   });
 });
+
+// @ponicode
+describe("sum", () => {
+    test("0", () => {
+        let callFunction = () => {
+            sum(1, -1)
+        }
+    
+        expect(callFunction).not.toThrow()
+    })
+
+    test("1", () => {
+        let callFunction = () => {
+            sum(10, 0.0)
+        }
+    
+        expect(callFunction).not.toThrow()
+    })
+
+    test("2", () => {
+        let callFunction = () => {
+            sum(-100, -1)
+        }
+    
+        expect(callFunction).not.toThrow()
+    })
+
+    test("3", () => {
+        let callFunction = () => {
+            sum(0, 0)
+        }
+    
+        expect(callFunction).not.toThrow()
+    })
+
+    test("4", () => {
+        let callFunction = () => {
+            sum(-10, 100)
+        }
+    
+        expect(callFunction).not.toThrow()
+    })
+
+    test("5", () => {
+        let callFunction = () => {
+            sum(undefined, undefined)
+        }
+    
+        expect(callFunction).not.toThrow()
+    })
+})
